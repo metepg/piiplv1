a7a039a380ff82e97f1a762176968db085dabb17
unknown <meteg66@gmail.com>
2021-01-18 21:33:55 +0200

    Added components: Header, Content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Header from './Components/Header/Header';
+import Content from './Components/Content/Content';
 
 function App() {
   return (
-    <div className="App"><h1>Hello</h1></div>
+    <>
+      <Header />
+      <Content />
+    </>
   );
 }
 
