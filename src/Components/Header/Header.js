76605dd98980b5004cb320326bd027f25878803c
unknown <meteg66@gmail.com>
2021-01-18 21:34:39 +0200

    Init Header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import './styles.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        The Piipl
+      </h1>
+      <p>Create your own illustration figures</p>
+      <hr />
+    </header>
+  );
+}
+
+export default Header;
