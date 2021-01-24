e6c25fc505d6138e861525fd31c1f50d2aaa9aff
unknown <meteg66@gmail.com>
2021-01-24 10:47:19 +0200

    FROM Double quotes TO single quotes

---
+++ src/Images/Upper Body/Thumbnails/Thumbnails.js
@@ -0,0 +1,13 @@
+import { ReactComponent as Shirt1 } from './thumb_shirt_1.svg';
+import { ReactComponent as Shirt2 } from './thumb_shirt_2.svg';
+import { ReactComponent as Shirt3 } from './thumb_shirt_3.svg';
+import { ReactComponent as Shirt4 } from './thumb_shirt_4.svg';
+import { ReactComponent as Shirt5 } from './thumb_shirt_5.svg';
+
+export default {
+  shirt_1: Shirt1,
+  shirt_2: Shirt2,
+  shirt_3: Shirt3,
+  shirt_4: Shirt4,
+  shirt_5: Shirt5,
+};
