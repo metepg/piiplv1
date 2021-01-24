2cd66b486cddac7997b3bed66407a2706abb4d76
unknown <meteg66@gmail.com>
2021-01-24 10:49:54 +0200

    New thumbnails for lower body

---
+++ src/Images/Lower Body/Thumbnails/Thumbnails.js
@@ -0,0 +1,13 @@
+import { ReactComponent as Pants1 } from './thumb_pants_1.svg';
+import { ReactComponent as Pants2 } from './thumb_pants_2.svg';
+import { ReactComponent as Pants3 } from './thumb_pants_3.svg';
+import { ReactComponent as Pants4 } from './thumb_pants_4.svg';
+import { ReactComponent as Pants5 } from './thumb_pants_5.svg';
+
+export default {
+  pants_1: Pants1,
+  pants_2: Pants2,
+  pants_3: Pants3,
+  pants_4: Pants4,
+  pants_5: Pants5,
+};
